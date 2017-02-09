6907a5ca6efe973dd0a3db6a96cc6b5104594975
Sam Hutton <samuelfhutton@gmail.com>
2017-02-09 21:34:13 +0000

    general bug and nit fixes

--- src/main.cc
+++ src/main.cc
@@ -1,14 +1,18 @@
 #include "map_stack.h"
 #include <iostream>
 #include <stack>
+#include <unordered_map>
 
 struct Resource {
-    bool Equals(Resource resource) {
-      return (resource.id == id);
-    }
-    Resource dependencies [0];
-    int dependencies_size = 0;
-    int id;
+ public:
+  const char* GetID() { return id_; }
+  bool operator==(Resource resource) {
+    return (resource.id == id);
+  }
+  std::vector<Resource> dependencies [0];
+  int dependencies_size = 0;
+ private:
+  const char* id_;
 };
 
 int main() {
@@ -16,21 +20,21 @@ int main() {
 
 void SubmitResourcesForLoading(Resource resource) {
   std::stack<Resource> not_loaded;
-  Array<Resource> loaded;
+  std::unordered_map<const char*, Resource> loaded;
   MapStack<int, Resource> dependency_chain;
 
   not_loaded.push(resource);
   Resource current_resource;
 
-  while(not_loaded.size() > 0) {
+  while (not_loaded.size() > 0) {
     current_resource = not_loaded.top();
     
     // if current resource has been added to the dependency chain,
     // it's dependencies have been submitted to be loaded,
     // so we can submit the current resource for loading
-    if(dependency_chain.Size() > 0 &&
-        dependency_chain.Peek().Equals(current_resource)) {
-      loaded.Add(current_resource);
+    if (dependency_chain.Size() > 0 &&
+        dependency_chain.Peek() == current_resource)) {
+      loaded.push(current_resource.GetID(), current_resource);
       not_loaded.pop();
       dependency_chain.Pop();
       // AddToLoadQueue(current_resource);
@@ -38,15 +42,14 @@ void SubmitResourcesForLoading(Resource resource) {
 
     // otherwise, add unloaded dependencies of the current resource
     // to the not_loaded stack
-    for(int i = 0; i < current_resource.dependencies_size; i++) {
-      Resource dependency = current_resource.dependencies[i];
+    for (const Resource& dependency : current_resource.dependencies) {
       // if dependency appears in the dependency chain,
       // there must be a dependency loop
-      if(dependency_chain.Contains(dependency) {
+      if (dependency_chain.Contains(dependency) {
         // bad things
         return;
       }
-      if(!loaded.Contains(dependency) {
+      if (!loaded.Contains(dependency) {
         not_loaded.push(dependency);
       }
     }
--- src/map_stack.h
+++ src/map_stack.h
@@ -9,31 +9,31 @@
 // Event manager that passes event info of type EventInfoType on Trigger
 template<typename KeyType, typename ValueType>
 class MapStack {
-  public:
-    void Push(const KeyType key, const ValueType* value) {
-      key_stack.push(key);
-      map[key] = value;
-    }
-    ValueType* Pop() {
-      int key = key_stack.pop();
-      ValueType* value = map[key];
-      map[key] = null;
-      return value;
-    }
-    ValueType* Peek() {
-      int key = key_stack.top();
-      ValueType* value = map[key];
-      return value;
-    }
-    bool Contains(KeyType key) {
-      return (map[key] != NULL);
-    }
-    int Size() {
-      return key_stack.size();
-    }
+ public:
+  void Push(KeyType key, ValueType* value) {
+    key_stack.push(key);
+    map[key] = value;
+  }
+  ValueType* Pop() {
+    ValueType* value = Peek();
+    key_stack.pop();
+    map.erase(key);
+    return value;
+  }
+  ValueType* Peek() {
+    KeyType key = key_stack.top();
+    ValueType* value = map[key];
+    return value;
+  }
+  bool Contains(KeyType key) {
+    return (map.count(key) > 0);
+  }
+  size_t Size() {
+    return key_stack.size();
+  }
   
-  private:
-    std::stack<int> key_stack;
-    unordered_map<KeyType, ValueType*> map;
+ private:
+  std::stack<KeyType> key_stack_;
+  std::unordered_map<KeyType, ValueType*> map_;
 };
 
