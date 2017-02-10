948b3545da5f596652378a1a1bc28326c25a8272
Sam Hutton <samuelfhutton@gmail.com>
2017-02-10 19:53:00 +0000

    reworked map_stack and fixed many bugs

--- src/main.cc
+++ src/main.cc
@@ -1,58 +1,67 @@
-#include "map_stack.h"
+#include "set_stack.h"
 #include <iostream>
 #include <stack>
 #include <string>
-#include <unordered_map>
+#include <vector>
 
 struct Resource {
+  Resource(std::string resource_id) {
+    id = resource_id;
+  }
   bool operator==(Resource resource) const {
     return (resource.id == id);
   }
-  std::vector<Resource> dependencies;
-  const std::string id;
+  bool IsLoadRequested() const {
+    return false;
+  }
+  std::vector<std::string> dependency_ids;
+  std::string id;
 };
 
 int main() {
 }
 
-void SubmitResourcesForLoading(Resource resource) {
-  std::stack<Resource> not_loaded;
-  std::unordered_map<std::string, Resource> loaded;
-  MapStack<std::string, Resource> dependency_chain;
+Resource GetResourceByID(std::string resource_id) {
+  return Resource("resource_1");
+}
 
-  not_loaded.push(resource);
-  Resource current_resource;
+void SubmitResourcesForLoading(std::string resource_id) {
+  std::stack<std::string> not_loaded;
+  SetStack<std::string> dependency_chain;
+
+  not_loaded.push(resource_id);
+  std::string current_resource_id;
 
   while (not_loaded.size() > 0) {
-    current_resource = not_loaded.top();
+    current_resource_id = not_loaded.top();
     
     // if current resource has been added to the dependency chain,
     // it's dependencies have been submitted to be loaded,
     // so we can submit the current resource for loading
     if (dependency_chain.Size() > 0 &&
-        *dependency_chain.Peek() == current_resource) {
-      loaded[current_resource.id] = current_resource);
+        dependency_chain.Top() == current_resource_id) {
       not_loaded.pop();
       dependency_chain.Pop();
       // AddToLoadQueue(current_resource);
-    }
-
-    // otherwise, add unloaded dependencies of the current resource
-    // to the not_loaded stack
-    for (const Resource& dependency : current_resource.dependencies) {
-      // if dependency appears in the dependency chain,
-      // there must be a dependency loop
-      if (dependency_chain.Contains(dependency.id)) {
-        // bad things
-        return;
+    } else {
+      // otherwise, add unloaded dependencies of the current resource
+      // to the not_loaded stack
+      Resource current_resource = GetResourceByID(current_resource_id);
+      for (const Resource& dependency : current_resource.dependency_ids) {
+        // if dependency appears in the dependency chain,
+        // // there must be a dependency loop
+        if (dependency_chain.Contains(dependency.id)) {
+          // bad things
+          return;
+        }
+        if (!GetResourceByID(dependency.id).IsLoadRequested()) {
+          not_loaded.push(dependency.id);
+        }
       }
-      if (loaded.count(dependency.id) > 0) {
-        not_loaded.push(dependency);
-      }
+      // once all dependencies have been submitted to be loaded,
+      // the current resource must be added to the dependency chain
+      // in order to catch dependency loops
+      dependency_chain.Push(current_resource.id);
     }
-    // once all dependencies have been submitted to be loaded,
-    // the current resource must be added to the dependency chain
-    // in order to catch dependency loops
-    dependency_chain.Push(current_resource.id, current_resource);
   }
 }
