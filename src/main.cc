17246bdbcff7770598cdf08cac68e5ba6504febd
Sam Hutton <samuelfhutton@gmail.com>
2017-02-10 23:21:31 +0000

    fixed multiple dependency bug in a neater way

--- src/main.cc
+++ src/main.cc
@@ -3,6 +3,7 @@
 #include <stack>
 #include <string>
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 class Resource {
@@ -13,17 +14,11 @@ class Resource {
   bool operator==(Resource resource) const {
     return (resource.id == id);
   }
-  bool LoadRequested() const {
-    return load_requested_;
-  }
   void RequestLoad() {
-    load_requested_ = true;
-    std::cout << "Resource Load Requested: " << id << "\n";
+    std::cout << "-- Resource Load Requested: " << id << " --\n";
   }
   std::vector<std::string> dependency_ids;
   std::string id;
- private:
-  bool load_requested_ = false;
 };
 
 std::unordered_map<std::string, Resource*> resources;
@@ -35,6 +30,7 @@ Resource* GetResourceByID(std::string resource_id) {
 void SubmitResourcesForLoading(std::string resource_id) {
   std::stack<std::string> not_loaded;
   SetStack<std::string> dependency_chain;
+  std::unordered_set<std::string> load_required;
 
   not_loaded.push(resource_id);
   std::string current_resource_id;
@@ -45,37 +41,35 @@ void SubmitResourcesForLoading(std::string resource_id) {
     current_resource = GetResourceByID(current_resource_id);
 
     // if current resource has been added to the dependency chain,
-    // it's dependencies have been submitted to be loaded,
+    // it's dependencies must already have been loaded
     // so we can submit the current resource for loading
     if (dependency_chain.Size() > 0 &&
         dependency_chain.Top() == current_resource_id) {
       not_loaded.pop();
       dependency_chain.Pop();
-      // load request is only skipped in the event that
-      // a resource dependency list contains two identical ids
-      if (!current_resource->LoadRequested()) {
-        current_resource->RequestLoad();
-      }
-    
+      current_resource->RequestLoad();
     } else {
-      // otherwise, add unloaded dependencies of the current resource
-      // to the not_loaded stack
+      // otherwise, mark fresh dependencies as not loaded
       for (const std::string dependency_id : current_resource->dependency_ids) {
         // if dependency appears in the dependency chain,
-        // // there must be a dependency loop
+        // there must be a dependency loop
         if (dependency_chain.Contains(dependency_id)) {
-          // bad things
-          std::cout << "Circular dependency detected. Exiting." << "\n";
+          std::cout << "-- Circular dependency detected: Exiting --\n";
           return;
         }
-        if (!(GetResourceByID(dependency_id)->LoadRequested())) {
+        // only mark a dependency id as not loaded if
+        // it isn't already marked as required
+        if (load_required.count(dependency_id) == 0) {
+          load_required.insert(dependency_id);
           not_loaded.push(dependency_id);
+          std::cout << "added " << dependency_id << " to not_loaded\n";
         }
       }
-      // once all dependencies have been submitted to be loaded,
+      // once all dependencies have been marked for loading,
       // the current resource must be added to the dependency chain
       // in order to catch dependency loops
       dependency_chain.Push(current_resource_id);
+      std::cout << "added " << current_resource_id << " to dependency_chain\n";
     }
   }
 }
